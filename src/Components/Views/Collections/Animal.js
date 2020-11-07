3a87b0f7c14df387ed1590650ece78bc61845d60
Richard Howell <rhowell@madmobile.com>
2020-11-07 08:22:24 -0500

    Saving Changes

--- src/Components/Views/Collections/Animal.js
+++ src/Components/Views/Collections/Animal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actionCreators from '../../../actions/index';
-
+import _ from 'lodash';
 import ReactHtmlParser from 'react-html-parser';
 import axios from 'axios';
 import ReactGA from 'react-ga';
@@ -12,33 +12,73 @@ class Animal extends Component {
 
 	componentDidMount() {}
 
+	displayRecords = (records) => {
+		records = '';
+		if (!!records.length) {
+			return <div className="collections-animal-quick-records">Quick Records</div>;
+		}
+	};
+
 	render() {
 		return (
 			<div className="collections-animal-page">
-				<div className="collection-animal-image">
-					<img src="images/perry.jpg" />
-				</div>
-				<div className="collection-animal-common-info">
-					<label>Name: </label>
-					<div>
-						<input type="text" name="name" placeholder="Perry" />
+				<div className="collection-animal-img-info">
+					<div className="collection-animal-image">
+						<img src="images/perry.jpg" />
 					</div>
-					<label>ID #: </label>
-					<div>
-						<input type="text" name="name" placeholder="5e1fb48ee2dd8f353c06d765" />
-					</div>
-					<div>
-						<label>Sex:</label>
-						<div>
-							<select>
-								<option>Male</option>
-								<option>Female</option>
-								<option>Unknown</option>
-							</select>
+					<div className="collection-animal-common-info">
+						<div className="collection-animal-name">
+							<label>Name:</label>
+							<div>
+								<input type="text" name="animalName" placeholder="" />
+							</div>
 						</div>
-						<label>DOB: </label>
+
+						<div className="collection-animal-id">
+							<label>ID #:</label>
+							<div>
+								<input type="text" name="animalID" placeholder="" />
+							</div>
+						</div>
+
+						<div className="collection-animal-dob">
+							<label>DOB:</label>
+							<div>
+								<input type="text" name="animalDOB" placeholder="" />
+							</div>
+						</div>
+
+						<div className="collection-animal-sire">
+							<label>Sire:</label>
+							<div>
+								<input type="text" name="animalSire" placeholder="" />
+							</div>
+						</div>
+
+						<div className="collection-animal-dam">
+							<label>Dam:</label>
+							<div>
+								<input type="text" name="animalDam" placeholder="" />
+							</div>
+						</div>
+						<div className="collection-animal-gender">
+							<label>Sex:</label>
+							<div className="collection-animal-sex">
+								<select>
+									<option>Male</option>
+									<option>Female</option>
+									<option>Unknown</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="collection-animal-body">
+					{this.displayRecords()}
+					<div className="collection-animal-comments">
+						<label>Comments:</label>
 						<div>
-							<input type="text" name="name" placeholder="5e1fb48ee2dd8f353c06d765" />
+							<textarea type="text" name="animalComment" placeholder="" />
 						</div>
 					</div>
 				</div>
