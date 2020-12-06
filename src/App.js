e7c3d653d1a912d8cf739f57f45f6abbb4b6a297
rhowell <rhowell@madmobile.com>
2020-12-06 00:13:03 -0500

    Saving Changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import axios from 'axios';
 import { Base64 } from 'js-base64';
 //import Footer from './Components/Modules/Footer/Footer';
 import ReactGA from 'react-ga';
-//import socket from './Components/_services/SocketService';
+import socket from './Components/_services/SocketService';
 
 ReactGA.initialize('UA-136119302-1');
 
@@ -24,11 +24,11 @@ class App extends Component {
 
 	componentDidMount() {
 		if (!!localStorage.token) {
-			// socket.on('connect', () => {
-			// 	socket.emit('newUser', {
-			// 		uid: this.props.userUid
-			// 	})
-			// })
+			socket.on('connect', () => {
+				socket.emit('newUser', {
+					uid: this.props.userUid,
+				});
+			});
 
 			this.props.getMyCollections({ uid: this.props.userUID });
 		}
